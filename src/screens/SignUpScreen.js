2e8032adda3940caa91ec1eaf01f4c5219cfe19a
techhype <krizzkishore@gmail.com>
2020-08-23 20:25:48 +0530

    SIgn Up DataBase Connection Successful

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,97 +1,141 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import { StyleSheet, Text, TextInput, Button, View, TouchableHighlight } from 'react-native';
 import moveToBottom from '../../library/utils/moveToBottom';
+import { set } from 'react-native-reanimated';
 
 const signUp = () => {
-    return (
-        <TouchableHighlight>
-            <View style={styles.button}>
-                <Text style={{ color: 'white', fontSize: 14 }}>Sign Up</Text>
-            </View>
-        </TouchableHighlight>
-    );
+	return (
+		<TouchableHighlight>
+			<View style={styles.button}>
+					<Text style={{ color: 'white', fontSize: 14 }}>Sign Up</Text>
+			</View>
+		</TouchableHighlight>
+	);
 };
 
 const SignUp = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <Text>Demo form</Text>
-            <View >
-                <TextInput placeholder="User Name" style={styles.inputStyle} />
-                <TextInput placeholder="Email" style={styles.inputStyle} />
-                <TextInput placeholder="Password" style={styles.inputStyle} secureTextEntry={true} />
-                <TextInput placeholder="Mobile Number" style={styles.inputStyle} />
-                {/*  Component Inheritance Not working */}
-                {/* <signUp /> */}
-                {
-                    moveToBottom(
-                        <Fragment>
-                            <TouchableHighlight onPress={() => navigation.navigate('Home')}>
-                                <View style={styles.button}>
-                                    <Text style={{ color: 'white', fontSize: 22, fontFamily: 'Inter Medium' }}>Sign Up</Text>
-                                </View>
-                            </TouchableHighlight>
-                            <TouchableHighlight onPress={() => navigation.navigate('Login')}>
-                                <View style={[styles.button, { backgroundColor: "#293448", borderColor: "#293448" }]}>
-                                    <Text style={{ color: 'white', fontSize: 22, fontFamily: 'Inter Medium' }}>Have Account?</Text>
-                                </View>
-                            </TouchableHighlight>
-                        </Fragment>
-                    )
-                }
-            </View>
-        </View>
-    );
+	const [username,setUsername] = useState('');
+	const [email,setEmail] = useState('');
+	const [password,setPassword] = useState('');
+	const [mnumber,setMnumber] = useState('');
+
+	// sends Credentials to Backend (Node.js) 
+	sendCredentials = ()=>{
+		fetch("http://14cd9722c5cd.ngrok.io/signup",{
+			method:'POST',
+			headers:{
+				'Content-Type': 'application/json'
+			},
+			body:JSON.stringify({
+				"username": username,
+				"email": email,
+				"password": password,
+				"mnumber": mnumber
+			})	
+		})
+		.then(res => res.json())
+		.then(data=>
+				console.log(data))
+	}
+	return (
+		<View style={styles.container}>
+			<Text>Demo form</Text>
+			<View >
+				<TextInput 
+					placeholder="User Name" 
+					style={styles.inputStyle}
+					value={username}
+					onChangeText={ (text)=>setUsername(text) } />
+				<TextInput 
+					placeholder="Email" 
+					style={styles.inputStyle}
+					value= {email}
+					onChangeText={ (text)=>setEmail(text) } />
+				<TextInput 
+					placeholder="Password" 
+					style={styles.inputStyle} 
+					secureTextEntry={true}
+					value= {password}
+					onChangeText={ (text)=>setPassword(text) } />
+				<TextInput 
+					placeholder="Mobile Number" 
+					style={styles.inputStyle}
+					value= {mnumber}
+					onChangeText={ (text)=>setMnumber(text) } />
+				{/*  Component Inheritance Not working */}
+				{/* <signUp /> */}
+				{
+					moveToBottom(
+							<Fragment>
+									<TouchableHighlight onPress={() => sendCredentials()}>
+										<View style={styles.button}>
+											<Text style={{ color: 'white', fontSize: 22, fontFamily: 'Inter Medium' }}>
+												Sign Up
+											</Text>
+										</View>
+									</TouchableHighlight>
+									<TouchableHighlight onPress={() => navigation.navigate('Login')}>
+										<View style={[styles.button, { backgroundColor: "#293448", borderColor: "#293448" }]}>
+											<Text style={{ color: 'white', fontSize: 22, fontFamily: 'Inter Medium' }}>
+												Have Account?
+											</Text>
+										</View>
+									</TouchableHighlight>
+							</Fragment>
+					)
+				}
+			</View>
+		</View>
+	);
 }
 
 const check = {
-    isAndroid: () => {
-        return Platform.OS == 'android'
-    }
+	isAndroid: () => {
+			return Platform.OS == 'android'
+	}
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 25,
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: 50,
-        paddingBottom: check.isAndroid ? 16 : 0,
-    },
-    inputStyle: {
-        margin: 20,
-        width: 300,
-        height: 40,
-        paddingLeft: 10,
-        paddingHorizontal: 10,
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: 'gray',
-    },
-    button: {
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: '#176CDC',
-        color: '#fff',
-        alignItems: "center",
-        backgroundColor: "#176CDC",
-        width: 355,
-        height:52,
-        marginTop: 10,
-        padding: 10,
-        // Shadow  
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.23,
-        shadowRadius: 2.62,
-        elevation: 4,
-    },
-
+	container: {
+			marginTop: 25,
+			flex: 1,
+			backgroundColor: '#fff',
+			alignItems: 'center',
+			justifyContent: 'center',
+			height: 50,
+			paddingBottom: check.isAndroid ? 16 : 0,
+	},
+	inputStyle: {
+			margin: 20,
+			width: 300,
+			height: 40,
+			paddingLeft: 10,
+			paddingHorizontal: 10,
+			borderRadius: 5,
+			borderWidth: 1,
+			borderColor: 'gray',
+	},
+	button: {
+			borderRadius: 5,
+			borderWidth: 1,
+			borderColor: '#176CDC',
+			color: '#fff',
+			alignItems: "center",
+			backgroundColor: "#176CDC",
+			width: 355,
+			height:52,
+			marginTop: 10,
+			padding: 10,
+			// Shadow  
+			shadowColor: '#000',
+			shadowOffset: {
+					width: 0,
+					height: 2,
+			},
+			shadowOpacity: 0.23,
+			shadowRadius: 2.62,
+			elevation: 4,
+	},
 
 });
 
