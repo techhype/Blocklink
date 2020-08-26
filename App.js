c61c5f33d391cbbbd91f05cd76944516765680b5
techhype <krizzkishore@gmail.com>
2020-08-26 12:23:41 +0530

    Changed to React  Navigation from v4 to v5

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
+import React from 'react';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const AppNavigator = createStackNavigator(
-  {
-    Home: {
-      screen:HomeScreen,
-      header: null,
-    },
-    Login: LoginScreen,
-    SignUp: SignUpScreen,
-    Welcome: {
-      screen:WelcomeScreen,
-      navigationOptions:{
-        headerShown:false,
-      }
-    }
-  },
-  {
-    initialRouteName: 'Welcome',
-  }
-);
+const Stack = createStackNavigator();
 
-export default createAppContainer(AppNavigator);
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen 
+          name="Welcome" 
+          component={WelcomeScreen}
+          options= {{headerShown:false}} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
