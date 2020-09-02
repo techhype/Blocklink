8c751f1b00432c8c931334344a6bf48ec39c7130
techhype <krizzkishore@gmail.com>
2020-09-02 19:33:13 +0530

    Changed to a new API and included Tab Navigation

---
+++ src/screens/Dashboard.js
@@ -0,0 +1,95 @@
+import React, {useState,useEffect} from 'react';
+import { Text, View, Button,Image,StatusBar, ScrollView, RefreshControl, StyleSheet } from 'react-native';
+import Card from '../../library/utils/Card';
+const imageData = require("../../assets/img/ImageData.json")
+
+
+
+const HomeScreen = ({ navigation }) => {
+    const initialValue = [{ name: '', price: '', change: ''}];
+    const [refreshing, setRefreshing] = React.useState(false);
+    const [stateOptions, setStateValues] = useState(initialValue);
+    
+    
+    useEffect(() => {
+        fetch("https://api.coinranking.com/v1/public/coins?ids=1,2,3")
+        .then((response) => response.json())
+        .then((json) => {
+            let coinData= [];
+            (json.data.coins).map(coin => {
+                let { name, price, change } = coin;
+                price = parseFloat(price).toFixed(2);
+                coinData.push({name,price: `${price} USD` ,change});
+            });
+            console.log(coinData);
+            setStateValues(coinData);
+        })
+        .catch((error) => console.error(error));
+    },[]);
+    
+    const wait = (timeout) => {
+        return new Promise(resolve => {
+            setTimeout(resolve, timeout);
+        });
+    }
+    
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        fetch("https://api.coinranking.com/v1/public/coins?ids=1,2,3")
+        .then((response) => response.json())
+        .then((json) => {
+            let coinData = [];
+            (json.data.coins).map(coin => {
+                let { name, price, change } = coin;
+                price = parseFloat(price).toFixed(2);
+                coinData.push({ name, price: `${price} USD`, change });
+            });
+            console.log(coinData);
+            setStateValues(coinData);
+        })
+        .catch((error) => console.error(error));
+        wait(1500).then(() => setRefreshing(false));
+    }, [refreshing]);
+
+    return (
+        <ScrollView style={ {
+                        flex:1,
+                        // alignItems: 'center',
+                        // justifyContent: 'center'
+                    } }
+            refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }>
+            <StatusBar backgroundColor= '#121d33'/>
+            {stateOptions.map((localState,index) => (
+                <Card key={index}>
+                    <View style={{width:200,borderRightWidth: 1 ,borderRightColor: '#D3D3D3'}}>
+                        <View style={{flexDirection: "row",marginBottom:30,}}>
+                            <Image
+                                style={{ width: 28, height: 28 }}
+                                source={{ uri: imageData[localState.name] }}
+                            />
+                            <Text style={{ fontSize: 20, marginLeft: 15,fontSize: 25, fontFamily: 'Inter'}}>{localState.name}</Text>
+                        </View>
+                        <Text style={{ marginLeft: 44,marginBottom: 5, fontFamily: 'Inter'}}>{localState.price}</Text>
+                        <Text style={[localState.change>0 ? styles.increase : styles.decrease,{ marginLeft: 44,marginBottom: 5, fontFamily: 'Inter'}]}>{localState.change}%</Text>
+                    </View>
+                </Card>
+            ))}
+            
+            <Button title='Log out' onPress={() => navigation.navigate('Login')} />
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    increase:{
+        color: 'green'
+    },
+    decrease:{
+        color: 'red'
+    }
+
+});
+
+export default HomeScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,90 +1,71 @@
-import React, {useState,useEffect} from 'react';
-import { Text, View, Button,Image,StatusBar, ScrollView, RefreshControl } from 'react-native';
-import Card from '../../library/utils/Card';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Dashboard from './Dashboard';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
-// const fetchCryptoDetails = async () =>{
-//     await fetch('https://sochain.com/api/v2/get_info/DOGE', {
-//         method: 'GET',
-//         headers: {
-//         'Content-Type': 'application/json',
-//         }
-//     })
-//         .then((res) => res.json())
-//         .then( (data) => {
-//         try {
-//             var string = JSON.stringify(data);
-//             var objectValue = JSON.parse(string);
-//             console.log(objectValue['data']['name']);
-//         } catch (e) {
-//             console.log('error:', e);
-//             // Alert(e)
-//         }
-//         });
-// }
-
-const HomeScreen = ({ navigation }) => {
-    const [refreshing, setRefreshing] = React.useState(false);
-    const [cryptoName,setCryptoName] = useState('');
-    const [cryptoValue,setCryptoValue] = useState('');
-
-    useEffect(() => {
-        fetch('https://sochain.com/api/v2/get_info/Bitcoin')
-            .then((response) => response.json())
-            .then((json) => {
-                setCryptoName(json.data.name);
-                let price = parseFloat(json.data.price).toFixed(2);
-                const cryptoValue = `${json.data.price_base} ${(price)}`;
-                setCryptoValue(cryptoValue);
-            })
-            .catch((error) => console.error(error))
-    },[]);
-
-    const wait = (timeout) => {
-        return new Promise(resolve => {
-            setTimeout(resolve, timeout);
-        });
-    }
-
-    const onRefresh = React.useCallback(() => {
-        setRefreshing(true);
-        fetch('https://sochain.com/api/v2/get_info/Bitcoin')
-            .then((response) => response.json())
-            .then((json) => {
-                setCryptoName(json.data.name);
-                let price = parseFloat(json.data.price).toFixed(2);
-                const cryptoValue = `${json.data.price_base} ${(price)}`;
-                setCryptoValue(cryptoValue);
-            })
-            .catch((error) => console.error(error));
-        wait(1500).then(() => setRefreshing(false));
-    }, [refreshing]);
-
-    return (
-        <ScrollView style={ {
-                        flex:1,
-                        // alignItems: 'center',
-                        // justifyContent: 'center'
-                    } }
-            refreshControl={
-                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-            }>
-            <StatusBar backgroundColor= '#121d33'/>
-            <Card>
-                <View style={{width:200,borderRightWidth: 1 ,borderRightColor: '#D3D3D3'}}>
-                    <View style={{flexDirection: "row",marginBottom:30,}}>
-                        <Image
-                            style={{ width: 28, height: 28 }}
-                            source={require('../../assets/img/bitcoin.png')}
-                        />
-                        <Text style={{ fontSize: 20, marginLeft: 15,fontSize: 25, fontFamily: 'Inter'}}>{cryptoName}</Text>
-                    </View>
-                    <Text style={{ marginLeft: 44,marginBottom: 5, fontFamily: 'Inter'}}>{cryptoValue}</Text>
-                </View>
-            </Card>
-            <Button title='Log out' onPress={() => navigation.navigate('Login')} />
-        </ScrollView>
-    );
+function SendCoin() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Send Coin!</Text>
+    </View>
+  );
 }
 
-export default HomeScreen;
+function ReceiveCoin() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Receive Coin!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function HomeScreen() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Dashboard"
+      tabBarOptions={{activeTintColor: '#0c6df2'}}>
+      <Tab.Screen
+        name="Send"
+        component={SendCoin}
+        options={{
+          tabBarLabel: 'Send',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="account-arrow-right-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={Dashboard}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Receive"
+        component={ReceiveCoin}
+        options={{
+          tabBarLabel: 'Receive',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="account-arrow-left-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
